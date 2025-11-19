25d84cc2a0a80693a1a4e3a6aeec18f73cef4598
Rahul Maurya <109868929+Rahulmaurya1234@users.noreply.github.com>
2025-11-19 05:14:56 -0800

    Add files via upload

---
+++ gallery.php
@@ -0,0 +1,44 @@
+<?php include('includes/header.php'); ?>
+
+<section class="work-section" id="work-section" style="background-color:white;">  
+    <div class ="work-gallery">
+            <div >
+                <h3 style="padding-top:10px;">Women Employment</h3>
+                <img src="images/women.png" alt="Photo 2" >
+            </div>
+            <div>
+                <h3 style="padding-top:10px;">clean India</h3>
+               <img src="images/clean.png" alt="Photo 2" >          
+
+            </div>
+            <div>
+                <h3 style="padding-top:10px;">Community Development</h3>
+                <img src="images/community.png" alt="Photo 2" >
+                
+
+                </div>
+     </div>
+        </div>
+
+        <div class ="work-gallery">
+            <div>
+            <h3 style="padding-top:10px;">Health and Wellness</h3>    
+            <img src="images/health.png" alt="Photo 2" >
+        </div>
+            <div>
+                <h3 style="padding-top:10px;">Education</h3>
+                <img src="images/education.png" alt="Photo 2" >
+             
+            </div>
+            <div>
+                <h3 style="padding-top:10px;">Girl Empowerment</h3>
+                <img src="images/second.png" alt="Photo 2" >
+             </div>
+             <div >
+        </div>
+       <button class="prev" onclick="prevSlide()">❮</button>
+       <button class="next" onclick="nextSlide()">❯</button>
+
+</section>
+
+<?php include('includes/footer.php'); ?>
